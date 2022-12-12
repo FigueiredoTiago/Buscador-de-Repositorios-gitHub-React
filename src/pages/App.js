9f631358983cd635f1b7c8b139765900dffb7474
tiago <canjhagamey@gmail.com>
2022-12-12 18:11:02 -0300

    Up

---
+++ src/pages/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import gitLogo from '../logos/github-mark-white.png'
+import Input from '../components/Input';
+import Button from '../components/Button';
+import ItemRepo from '../components/ItemRepo';
+import { api } from '../services/api';
+
+import { Container } from './style';
+
+function App() {
+
+  const [currentRepo, setCurrentRepo] = useState('');
+  const [repos, setRepos] = useState([]);
+
+
+  const handleSearchRepo = async () => {
+
+    const { data } = await api.get(`repos/${currentRepo}`)
+
+    if (data.id) {
+
+      const isExist = repos.find(repo => repo.id === data.id);
+
+      if (!isExist) {
+        setRepos(prev => [...prev, data]);
+        setCurrentRepo('')
+        return
+      }
+
+    }
+    alert('Repositório não encontrado')
+
+  }
+
+  const handleRemoveRepo = (id) => {
+    console.log('Removendo registro', id);
+
+    // utilizar filter.
+  }
+
+
+  return (
+    <Container>
+      <img src={gitLogo} width={72} height={72} alt="github logo" />
+      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
+      <Button onClick={handleSearchRepo} />
+      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo} />)}
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    body {
+        background-color: #22272e;
+        color: #fff ;
+    }
+
+
+
+
+
+
+`
